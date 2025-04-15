8fda5fcc00418b06bfa262fd60d656fbfc8a3f74
Ekzzzzzz <mariadelosangeles505@hotmail.com>
2025-04-14 23:47:51 -0500

    .
    .

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.btn-primary');
-  
-    buttons.forEach(button => {
-      button.addEventListener('click', (event) => {
-        event.preventDefault();
-        alert('Sección en desarrollo. ¡Gracias por tu interés!');
-      });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  // Inicializar tooltips de Bootstrap
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+
+  // Inicializar dropdowns de Bootstrap
+  var dropdownToggle = document.querySelectorAll('.dropdown-toggle');
+  dropdownToggle.forEach(function (dropdown) {
+      new bootstrap.Dropdown(dropdown);
+  });
+
+  // Actualizar el placeholder del input al hacer clic en una opción del menú
+  var dropdownItems = document.querySelectorAll('.dropdown-item');
+  dropdownItems.forEach(function (item) {
+      item.addEventListener('click', function (event) {
+          var input = document.getElementById('lugarInteres');
+          input.placeholder = event.target.textContent.trim(); // Actualiza el placeholder del input
+      });
+  });
+});
